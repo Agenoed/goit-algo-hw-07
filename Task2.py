342d1e712bed4decf7d11919038197c1a5790ded
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2024-11-03 16:34:09 +0200

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,41 @@
+class root:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return root(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def find_min(root):
+    if root is None:
+        return None
+    while root.left is not None:
+        root = root.left
+    return root.val
+
+# Test
+root = root(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+print("Найменше значення:", find_min(root)) 
